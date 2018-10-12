c3c6796d5a0f22ccc0d36d1e9d2dffc2f49670c4
Arthur Caillaud <arthur@Arthur-Caillaud.local>
2018-10-12 11:48:30 +0200

    Latest version of backend

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, request
 from document import Document
 from flask_socketio import SocketIO, send, emit
-app = Flask(__name__)
 
-socketio = SocketIO(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, host='localhost', port=5000)
+
+print('App running...')
 
 socketio.run(app)
 
-@socketio.on('connect', namespace='/connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
+def ack():
     print('Client Connected')
 
-@socketio.on('disconnect', namespace='/connect')
-def test_disconnect():
-    print('Client disconnected')
+@socketio.on('connect', namespace='/connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'}, cb=ack)
 
-@socketio.on('draw', namespace='/draw')
-def handle_draw(message):
-    print(message)
-    send(message)
-
-
+# @socketio.on('disconnect', namespace='/connect')
+# def test_disconnect():
+#     print('Client disconnected')
+#
+# @socketio.on('draw', namespace='/draw')
+# def handle_draw(message):
+#     print(message)
+#     send(message)
