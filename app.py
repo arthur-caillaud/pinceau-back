162e8ce7cd5dcafc59a26e34c7608a2d2fbe449b
JeanPatrik <maxime.dieudonne@student.ecp.fr>
2018-10-12 10:24:23 +0200

    new commit

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ from document import Document
 from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 socketio.run(app)
 
-@socketio.on('connect', namespace='/chat')
+@socketio.on('connect', namespace='/connect')
 def test_connect():
     emit('my response', {'data': 'Connected'})
+    print('Client Connected')
 
-@socketio.on('disconnect', namespace='/chat')
+@socketio.on('disconnect', namespace='/connect')
 def test_disconnect():
     print('Client disconnected')
 
-@socketio.on('draw')
+@socketio.on('draw', namespace='/draw')
 def handle_draw(message):
+    print(message)
     send(message)
 
 
