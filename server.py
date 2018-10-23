9cf49691ed7ddce424bd0de145e34ae305662211
Arthur Caillaud <arthurcaillaud@gmail.com>
2018-10-23 16:54:01 +0200

    Better variable names

--- server.py
+++ server.py
@@ -20,13 +20,13 @@ class Server(Thread):
         while True:
             asked_connexions, asked_write, exceptional = select.select([self.__connexion], [], [], 0.05)
             self.handle_asked_connexions(asked_connexions)
-            clients = []
+            sending_clients = []
             try:
-                clients, wlist, xlist = select.select(self.__connected_clients, [], [], 0.05)
+                sending_clients, wlist, xlist = select.select(self.__connected_clients, [], [], 0.05)
             except select.error:
                 pass
             else:
-                for sending_client in clients:
+                for sending_client in sending_clients:
                     self.handle_request(sending_client)
 
     def handle_asked_connexions(self, asked_connexions):
