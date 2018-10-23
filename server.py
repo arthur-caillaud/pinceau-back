914c8d8330f10c18235237594f6556ed09b017a3
Arthur Caillaud <arthurcaillaud@gmail.com>
2018-10-23 16:51:19 +0200

    First version with db

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from threading import Thread
 import socket
 import select
 import json
+import db
 
 class Server(Thread):
     def __init__(self, host, port):
@@ -12,14 +13,13 @@ class Server(Thread):
         self.__connected_clients = []
         self.__connexion.bind((self.__host, self.__port))
         self.__connexion.listen(100)
+        self.__db = db('localhost', 'pinceau', 'password')
         print("Server now listening on port {}".format(self.__port))
 
     def run(self):
         while True:
             asked_connexions, asked_write, exceptional = select.select([self.__connexion], [], [], 0.05)
-            for client in asked_connexions:
-                client_connexion, connexion_infos = self.__connexion.accept()
-                self.__connected_clients.append(client_connexion)
+            self.handle_asked_connexions(asked_connexions)
             clients = []
             try:
                 clients, wlist, xlist = select.select(self.__connected_clients, [], [], 0.05)
@@ -27,9 +27,33 @@ class Server(Thread):
                 pass
             else:
                 for sending_client in clients:
-                    message = json.loads(sending_client.recv(1024))
-                    if message["action"] == "disconnect":
-                        sending_client.close()
-                    else:
-                        for recipient_client in self.__connected_clients:
-                            recipient_client.send(json.dumps(message).encode())
+                    self.handle_request(sending_client)
+
+    def handle_asked_connexions(self, asked_connexions):
+        for client in asked_connexions:
+            client_connexion, connexion_infos = self.__connexion.accept()
+            self.__connected_clients.append(client_connexion)
+
+    def handle_request(self, sending_client):
+        request = json.loads(sending_client.recv(1024))
+        if request['action'] == 'disconnect':
+            self.handle_disconnect(sending_client)
+        if request['action'] == 'connect':
+            self.handle_connect(sending_client)
+        else:
+            self.transmit_shape(request)
+
+    def handle_disconnect(self, sending_client):
+        sending_client.close()
+
+    def handle_connect(self, sending_client):
+        shapes = self.__db.select_all_shapes()
+        for shape in shapes:
+            message = json.dumps(shape).encode()
+            sending_client.send(message)
+
+    def transmit_shape(self, shape):
+        for recipient_client in self.__connected_clients:
+            self.__db.insert_shape(shape)
+            message = json.dumps(shape).encode()
+            recipient_client.send(message)
