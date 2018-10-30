cec42b6f2c3188bf3f7c5a0fc1e3cd42590c1622
Arthur Caillaud <arthurcaillaud@gmail.com>
2018-10-30 14:44:22 +0100

    Sending shapes array

--- server.py
+++ server.py
@@ -17,12 +17,13 @@ class Server(Thread):
 
     def run(self):
         print("Server now listening on port {}".format(self.__port))
+        connexion, connected_clients = self.get_connexion(), self.get_connected_clients()
         while True:
-            asked_connexions, asked_write, exceptional = select.select([self.__connexion], [], [], 0.05)
+            asked_connexions, asked_write, exceptional = select.select([connexion], [], [], 0.05)
             self.handle_asked_connexions(asked_connexions)
             sending_clients = []
             try:
-                sending_clients, wlist, xlist = select.select(self.__connected_clients, [], [], 0.05)
+                sending_clients, wlist, xlist = select.select(connected_clients, [], [], 0.05)
             except select.error:
                 pass
             else:
@@ -30,9 +31,10 @@ class Server(Thread):
                     self.handle_request(sending_client)
 
     def handle_asked_connexions(self, asked_connexions):
+        connexion, connected_clients = self.get_connexion(), self.get_connected_clients()
         for client in asked_connexions:
-            client_connexion, connexion_infos = self.__connexion.accept()
-            self.__connected_clients.append(client_connexion)
+            client_connexion, connexion_infos = connexion.accept()
+            connected_clients.append(client_connexion)
 
     def handle_request(self, sending_client):
         request = json.loads(sending_client.recv(1024))
@@ -47,12 +49,23 @@ class Server(Thread):
         sending_client.close()
 
     def handle_connect(self, sending_client):
-        for shape in self.__shapes:
-            message = json.dumps(shape).encode()
-            sending_client.send(message)
+        shapes = self.get_shapes()
+        message = json.dumps(shapes).encode()
+        sending_client.send(message)
 
     def transmit_shape(self, shape):
-        self.__shapes.append(shape)
-        for recipient_client in self.__connected_clients:
-            message = json.dumps(shape).encode()
+        shapes, connected_clients = self.get_shapes(), self.get_connected_clients()
+        shapes.append(shape)
+        for recipient_client in connected_clients:
+            message = json.dumps([shape]).encode()
             recipient_client.send(message)
+
+    # Getters
+    def get_connexion(self):
+        return self.__connexion
+
+    def get_connected_clients(self):
+        return self.__connected_clients
+
+    def get_shapes(self):
+        return self.__shapes
