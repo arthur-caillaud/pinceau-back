83a85d97b0535d42915eaa967eb2cd9e0efea17d
Arthur Caillaud <arthurcaillaud@gmail.com>
2018-11-08 19:54:32 +0100

    server.py was useless

--- app.py
+++ app.py
@@ -1,3 +1,62 @@
-from server import Server
+# External modules
+from threading import Thread
+import socket
+import select
+import json
+
+class Server(Thread):
+    def __init__(self, port=5000):
+        Thread.__init__(self)
+        self.__host = ''
+        self.__port = port
+        self.__connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__connexion.bind((self.__host, self.__port))
+        self.__connexion.listen(100)
+        self.__actions = []
+        self.__connected_clients = []
+
+    def run(self):
+        print('Server now listening on port {}'.format(self.__port))
+        while True:
+            asked_connexions, asked_write, exceptional = select.select([self.__connexion], [], [], 0.05)
+            self.handle_asked_connexions(asked_connexions)
+            sending_clients = []
+            try:
+                sending_clients, wlist, xlist = select.select(self.__connected_clients, [], [], 0.05)
+                for sending_client in sending_clients:
+                    self.handle_request(sending_client)
+            except select.error:
+                pass
+
+    def handle_asked_connexions(self, asked_connexions):
+        for client in asked_connexions:
+            client_connexion, connexion_infos = self.__connexion.accept()
+            print('New client connected.', connexion_infos)
+            self.__connected_clients.append(client_connexion)
+
+    def handle_request(self, sending_client):
+        request = json.loads(sending_client.recv(1024))
+        if request['action'] == 'disconnect':
+            self.handle_disconnect(sending_client)
+        if request['action'] == 'sync':
+            self.handle_sync(sending_client)
+        if request['action'] == 'add' or request['action'] == 'erase':
+            self.transmit_action(sending_client, request)
+
+    def handle_disconnect(self, sending_client):
+        sending_client.close()
+        self.__connected_clients.remove(sending_client)
+        print('Client disconnected.')
+
+    def handle_sync(self, sending_client):
+        message = json.dumps(self.__actions).encode()
+        sending_client.send(message)
+
+    def transmit_action(self, sending_client, action):
+        self.__actions.append(action)
+        for recipient_client in self.__connected_clients:
+            if sending_client != recipient_client:
+                message = json.dumps([action]).encode()
+                recipient_client.send(message)
 
 Server().start()
